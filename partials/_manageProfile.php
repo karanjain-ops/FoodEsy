bfb6d86e20d3c985d2c6a9f08448e3b21c8ca427
karanjain-ops <jkaran.2349@gmail.com>
2022-11-05 15:59:57 +0530

    undo profile changes

--- partials/_manageProfile.php
+++ partials/_manageProfile.php
@@ -3,6 +3,31 @@
     session_start();
     $userId = $_SESSION['userId'];
     
+    
+    if(isset($_POST["updateProfilePic"])){
+        $check = getimagesize($_FILES["image"]["tmp_name"]);
+        if($check !== false) {
+            $newfilename = "person-".$userId.".jpg";
+
+            $uploaddir = $_SERVER['DOCUMENT_ROOT'].'/FoodEsy/img/';
+            $uploadfile = $uploaddir . $newfilename;
+
+            if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
+                echo "<script>alert('Success!!');
+                        window.location=document.referrer;
+                    </script>";
+            } else {
+                echo "<script>alert('Image Upload Failed!! Please Try Again.');
+                        window.location=document.referrer;
+                    </script>";
+            }
+        }
+        else{
+            echo '<script>alert("Please select an Image file to upload.");
+                window.history.back(1);
+            </script>';
+        }
+    }
 
     if(isset($_POST["updateProfileDetail"])){
         $firstName = $_POST["firstName"];
@@ -31,6 +56,21 @@
             echo '<script>alert("Password is incorrect!!");
                         window.history.back(1);
                     </script>';
+        }
+    }
+    
+    if(isset($_POST["removeProfilePic"])){
+        $filename = $_SERVER['DOCUMENT_ROOT']."/FoodEsy/img/person-".$userId.".jpg";
+        if (file_exists($filename)) {
+            unlink($filename);
+            echo "<script>alert('Removed!!');
+                window.location=document.referrer;
+            </script>";
+        }
+        else {
+            echo "<script>alert('No Photo Available.');
+                window.location=document.referrer;
+            </script>";
         }
     }
     
