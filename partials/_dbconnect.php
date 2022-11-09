b9d0657f0eccd60ee2ae5af1aa14b123d2f8d56c
karanjain-ops <jkaran.2349@gmail.com>
2022-11-10 01:50:24 +0530

    deploy db

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,8 +1,14 @@
 <?php
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "OFD";
+// $server = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "OFD";
+
+$server= "remotemysql.com";
+$username = "5OjmcHoZpi";
+$password = "IW2is90WJb";
+$database = "5OjmcHoZpi";
+
 
 $conn = mysqli_connect($server, $username, $password, $database);
 if (!$conn){
